2f5d3363d340dc8642bf4237bb2773e21afbca70
Shahg-ops <ahsenarshad43@gmail.com>
2026-02-12 14:16:10 +0500

    Change color from pink to red in animation

--- script/main.js
+++ script/main.js
@@ -222,7 +222,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "red",
         ease: Expo.easeOut,
       },
       0.1,
